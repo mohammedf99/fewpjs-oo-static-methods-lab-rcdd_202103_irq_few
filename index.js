0c22c72c0f503bac2e0d24ba0ce233bcfc132a03
Mohammed Mahmood <mohammed.hamidf99@gmail.com>
2021-05-16 16:19:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,8 +9,22 @@ class Formatter {
     return str.replace(/[^A-Za-z0-9-']+/g, '');
   }
   
-  static titleize(str) {
-    const words = str.trim().split('');
-      return words.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+static titleize(sentence) {
+    const except= ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    let result = [];
+    let array = sentence.split(" ");
+
+    for (let i = 0; i < array.length; i++) {
+      if (i === 0) {
+        result.push(this.capitalize(array[i]))
+      } else {
+        if (except.includes(array[i])) {
+          result.push(array[i])
+        } else {
+          result.push(this.capitalize(array[i]))
+        }
+      }
+    }
+    return result.join(" ");
   }
 }
